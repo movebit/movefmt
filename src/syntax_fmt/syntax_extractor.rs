4ba8cbbde9fb66eff2628bd03412f7e9143c3fc3
hapeeeeee <623151737@qq.com>
2025-03-26 13:18:05 +0800

    abstract syntax extractor trait

--- src/syntax_fmt/syntax_extractor.rs
+++ src/syntax_fmt/syntax_extractor.rs
@@ -1,4 +1,13 @@
+use move_compiler::parser::ast::*;
 
 pub trait SyntaxExtractor {
-    
+    fn collect_seq_item(&mut self, s: &SequenceItem);
+    fn collect_seq(&mut self, s: &Sequence);
+    fn collect_spec(&mut self, spec_block: &SpecBlock);
+    fn collect_expr(&mut self, e: &Exp);
+    fn collect_const(&mut self, c: &Constant);
+    fn collect_function(&mut self, d: &Function);
+    fn collect_module(&mut self, d: &ModuleDefinition);
+    fn collect_script(&mut self, d: &Script);
+    fn collect_definition(&mut self, d: &Definition);
 }
