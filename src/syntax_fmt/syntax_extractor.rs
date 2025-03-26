6b03016f9d63151b59bd9f37f83146e494a63634
hapeeeeee <623151737@qq.com>
2025-03-26 13:42:23 +0800

    update SingleSyntaxExtractor trait

--- src/syntax_fmt/syntax_extractor.rs
+++ src/syntax_fmt/syntax_extractor.rs
@@ -1,11 +1,13 @@
 use move_compiler::parser::ast::*;
 
-pub trait SyntaxExtractor {
+pub trait SingleSyntaxExtractor {
+    fn new(fmt_buffer: String) -> Self;
     fn collect_seq_item(&mut self, s: &SequenceItem);
     fn collect_seq(&mut self, s: &Sequence);
     fn collect_spec(&mut self, spec_block: &SpecBlock);
     fn collect_expr(&mut self, e: &Exp);
     fn collect_const(&mut self, c: &Constant);
+    fn collect_struct(&mut self, s: &StructDefinition);
     fn collect_function(&mut self, d: &Function);
     fn collect_module(&mut self, d: &ModuleDefinition);
     fn collect_script(&mut self, d: &Script);
