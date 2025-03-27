db25973cd476284aed58503b8966395566989776
hapeeeeee <623151737@qq.com>
2025-03-27 14:30:31 +0800

    single test for issue 49

--- tests/fmt_test.rs
+++ tests/fmt_test.rs
@@ -51,8 +51,7 @@ fn test_single_file() {
         .with_env_filter(EnvFilter::from_env("MOVEFMT_LOG"))
         .init();
 
-    // test_on_file(Path::new("./tests/bug2/issue49/input1.move"));
-    test_on_file(Path::new("/data/zhangxiao/move/movefmt/tests/complex6/input1.move"));
+    test_on_file(Path::new("./tests/bug2/issue49/input1.move"));
 }
 // (            registry_ref_mut, melee_is_active, participant_address, escrow_ref_mut, melee_id
 // (registry_ref_mut, melee_is_active, participant_address, escrow_ref_mut, melee_id
