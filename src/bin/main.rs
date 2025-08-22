364947f5ce550cdca4880beacb6d42a0edcbb377
robinlzw <849840504@qq.com>
2025-08-22 10:49:59 +0800

    update doc and version to #v1.3.0

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -497,7 +497,7 @@ fn print_usage_to_stdout(opts: &Options, reason: &str) {
 }
 
 fn print_version() {
-    println!("movefmt v1.2.9");
+    println!("movefmt v1.3.0");
 }
 
 fn determine_operation(matches: &Matches) -> Result<Operation, OperationError> {
