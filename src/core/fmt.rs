6a78cbc62b22ad599bc772ad85ee1fcf264124d8
qpzmV <849840504@qq.com>
2025-12-15 05:13:33 -0500

    refactor(fmt): switch FormatContext and Format to borrow input string instead of clone

--- src/core/fmt.rs
+++ src/core/fmt.rs
@@ -33,8 +33,8 @@ const MAX_ANALYZE_LENGTH: usize = 64;
 const MIN_BREAK_LENGTH: usize = 32;
 const MIN_NESTED_LENGTH: usize = 16;
 
-pub struct FormatContext {
-    pub content: String,
+pub struct FormatContext<'a> {
+    pub content: &'a str,
     pub pre_simple_token: TokenTree,
     pub pre_token_tree: TokenTree,
     pub cur_nested_kind: NestKind,
@@ -42,8 +42,8 @@ pub struct FormatContext {
     pub has_spec: bool,
 }
 
-impl FormatContext {
-    pub fn new(content: String) -> Self {
+impl<'a> FormatContext<'a> {
+    pub fn new(content: &'a str) -> Self {
         FormatContext {
             content,
             pre_simple_token: TokenTree::default(),
@@ -59,7 +59,7 @@ impl FormatContext {
     }
 }
 
-pub struct Format {
+pub struct Format<'a> {
     pub(crate) local_cfg: FormatConfig,
     pub(crate) global_cfg: Config,
     pub(crate) depth: Cell<usize>,
@@ -69,7 +69,7 @@ pub struct Format {
     pub(crate) comments_index: Cell<usize>,
     pub(crate) ret: RefCell<String>,
     pub(crate) cur_line: Cell<u32>,
-    pub(crate) format_context: RefCell<FormatContext>,
+    pub(crate) format_context: RefCell<FormatContext<'a>>,
     pub(crate) syntax_handler: SyntaxHandler,
 }
 
@@ -176,8 +176,8 @@ fn tune_module_buf(module_body: &mut String, config: &Config, has_spec: bool) {
     remove_trailing_whitespaces(module_body);
 }
 
-impl Format {
-    fn new(global_cfg: Config, content: &str, format_context: FormatContext) -> Self {
+impl<'a> Format<'a> {
+    fn new(global_cfg: Config, content: &str, format_context: FormatContext<'a>) -> Self {
         let ce: CommentExtrator = CommentExtrator::new(content).unwrap();
         let mut line_mapping = FileLineMappingOneFile::default();
         line_mapping.update(content);
@@ -2034,7 +2034,7 @@ impl Format {
     }
 }
 
-impl Format {
+impl<'a> Format<'a> {
     fn inc_depth(&self) {
         let old = self.depth.get();
         self.depth.set(old + 1);
@@ -2152,7 +2152,7 @@ impl Format {
     }
 }
 
-impl Format {
+impl<'a> Format<'a> {
     fn get_kind_len_after_trim_space(&self, kind: &NestKind) -> usize {
         self.format_context.borrow().content[kind.start_pos as usize..kind.end_pos as usize]
             .replace('\n', "")
@@ -2264,11 +2264,7 @@ pub fn format_entry(content: impl AsRef<str>, config: Config) -> Result<String, 
     // https://github.com/movebit/movefmt/issues/2
     let (defs, _) = parse_file_string(&mut get_compile_env(), FileHash::empty(), content)?;
 
-    let mut full_fmt = Format::new(
-        config.clone(),
-        content,
-        FormatContext::new(content.to_string()),
-    );
+    let mut full_fmt = Format::new(config.clone(), content, FormatContext::new(content));
 
     full_fmt.generate_token_tree(defs, content)?;
     timer = timer.done_parsing();
